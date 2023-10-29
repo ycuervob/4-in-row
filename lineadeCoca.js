d3ed3fcfcc3610d11ecd1e5f2607c692e2156699
yeison <ycuervob@unal.edu.co>
2023-10-29 16:54:55 -0500

    Paso minmax

--- lineadeCoca.js
+++ lineadeCoca.js
@@ -1,47 +1,57 @@
-class LineaDCoca extends Agent{
-    
-    constructor(){ 
+class LineaDCoca extends Agent {
+
+    constructor() {
         super();
-        this.board = new Board();
+        this.boardManager = new Board();
     }
 
-    init(color, board, time=20000){
+    init(color, board, time = 20000) {
         this.color = color
         this.noColor = color == "W" ? "B" : "W";
         this.time = time
         this.size = board.length
+        this.boardMatrix = board;
     }
-    
-    compute(board,time){
-        console.log(board,"tiempo",time);
 
-        var moves = this.board.valid_moves(board)
-        const index = this.minimax(board,moves,true);
+    compute(board, time) {
+        console.log(board, "tiempo", time);
+
+        var moves = this.boardManager.valid_moves(board)
+        const index = this.pasoMinimax(board, moves, true);
 
         console.log(this.color + ',' + moves[index])
         return moves[index]
     }
 
-    evaluate(board){
-        move
+    evaluate(board,isMaximizingPlayer) {
+        let boardScore = 0;
+
+
+
+        return boardScore
     }
 
-    // Función Minimax para encontrar la mejor jugada
-    minimax(board,moves, isMaximizingPlayer) {
+    //Solo hace un paso del minimax
+    pasoMinimax(board, moves, isMaximizingPlayer) {
 
         let index = 0;
+        let colorminmax = isMaximizingPlayer ? this.color : this.noColor;
+        let bestScore = isMaximizingPlayer ? -Infinity : +Infinity;
+        let minmaxEq = (isMaximizingPlayer, score, bestScore) => {
+            return isMaximizingPlayer ? score >= bestScore : score <= bestScore;
+        }
 
-        let bestScore = -Infinity;
-            for (let i = 0; i < moves.length; i++) {
-                if (moves[i]) {
-                    board.move(board,moves[i],this.color);
-                    const score = this.evaluate(board);
-                    if (score >= bestScore){
-                        index = i;
-                        bestScore = score;
-                    }
+        for (let i = 0; i < moves.length; i++) {
+            if (moves[i]) {
+                const cloneForPasoMinMax = this.boardManager.clone(board);
+                this.boardManager.move(cloneForPasoMinMax, moves[i], colorminmax);
+                const score = this.evaluate(cloneForPasoMinMax, isMaximizingPlayer);
+                if (minmaxEq(isMaximizingPlayer, score, bestScore)) {
+                    index = i;
+                    bestScore = score;
                 }
             }
+        }
         return index
     }
 }
