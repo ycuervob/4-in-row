9a7fd0a0adaff144cb25869f2e70a17f61fb6223
yeison <ycuervob@unal.edu.co>
2023-10-29 19:15:00 -0500

    Se completó en teória el paso minmax

--- lineadeCoca.js
+++ lineadeCoca.js
@@ -11,24 +11,84 @@ class LineaDCoca extends Agent {
         this.time = time
         this.size = board.length
         this.boardMatrix = board;
+        this.k = parseInt(Konekti.vc('k').value); // k-pieces in row
     }
 
     compute(board, time) {
-        console.log(board, "tiempo", time);
-
         var moves = this.boardManager.valid_moves(board)
-        const index = this.pasoMinimax(board, moves, true);
+        const index = this.minimax(board, moves, true);
 
         console.log(this.color + ',' + moves[index])
         return moves[index]
     }
 
-    evaluate(board,isMaximizingPlayer) {
-        let boardScore = 0;
+    conflictos(board, isMaximizingPlayer) {
+        if (tablero[i] == tablero[j] || Math.abs(tablero[i] - tablero[j]) == Math.abs(i - j)) {
+            c++;
+        }
+    }
+
+    evaluate(board, isMaximizingPlayer) {
+        let n = board.length;
+        let colorminmax = isMaximizingPlayer ? this.color : this.noColor;
+        const patronPunto = new RegExp(`[${colorminmax} ]{${this.k},}`, "g")
+        let vertical = [];
+        let horizontal = [];
+        let diagonalI = [];
+        let diagonalS = [];
+
+        let conteoVertical = 0;
+        let conteoHorizontal = 0;
+        let conteoDiagonalI = 0;
+        let conteoDiagonalS = 0;
 
 
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                vertical.push(board[i][j]);
+                horizontal.push(board[j][i]);
 
-        return boardScore
+                if (i + j < n) {
+                    diagonalI.push(board[i][i + j]);
+                    diagonalS.push(board[i + j][i]);
+                }
+
+            }
+
+            let verticalStr = vertical.join("");
+            let horizontalStr = horizontal.join("");
+            let diagonalIStr = diagonalI.join("");
+            let diagonalSStr = diagonalS.join("");
+
+            const coincidenciaVer = verticalStr.match(patronPunto);
+            const coincidenciaHor = horizontalStr.match(patronPunto);
+            const coincidenciaDiagI = diagonalIStr.match(patronPunto);
+            const coincidenciaDiagS = diagonalSStr.match(patronPunto);
+
+            if (coincidenciaVer) {
+                conteoVertical += coincidenciaVer.filter((char) => char == colorminmax).length;
+            }
+
+            if (coincidenciaHor) {
+                conteoHorizontal += coincidenciaHor.filter((char) => char == colorminmax).length;
+            }
+
+            if (coincidenciaDiagI) {
+                conteoDiagonalI += coincidenciaDiagI.filter((char) => char == colorminmax).length;
+            }
+
+            if (coincidenciaDiagS) {
+                conteoDiagonalS += coincidenciaDiagS.filter((char) => char == colorminmax).length;
+            }
+
+            vertical = []; horizontal = []; diagonalI = []; diagonalS = [];
+        }
+
+        return (conteoVertical + conteoHorizontal + conteoDiagonalS + conteoDiagonalI);
+    }
+
+    minimax(board, moves, isMaximizingPlayer) {
+
     }
 
     //Solo hace un paso del minimax
@@ -45,7 +105,7 @@ class LineaDCoca extends Agent {
             if (moves[i]) {
                 const cloneForPasoMinMax = this.boardManager.clone(board);
                 this.boardManager.move(cloneForPasoMinMax, moves[i], colorminmax);
-                const score = this.evaluate(cloneForPasoMinMax, isMaximizingPlayer);
+                const score = this.evaluate(cloneForPasoMinMax, isMaximizingPlayer, moves);
                 if (minmaxEq(isMaximizingPlayer, score, bestScore)) {
                     index = i;
                     bestScore = score;
