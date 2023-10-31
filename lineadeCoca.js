754b5f10a8135669d1b39166042e68d64d5def73
ycuervob <ycuervob@unal.edu.co>
2023-10-31 03:24:13 -0500

    Se dividen el algoritmo de alfa beta en dos funciones como en la definición
    También se me ocurrió incrementar el arbol de búsqueda con respecto al tamaño de fichas consecutivas validas para punto que tenga el adversario o con respecto a la columna mas llena

--- lineadeCoca.js
+++ lineadeCoca.js
@@ -5,6 +5,45 @@ class LineaDCoca extends Agent {
         this.boardManager = new Board();
     }
 
+    winner(board, k){
+        var size = board.length
+        for( var i=0; i<size; i++){
+            for(var j=0; j<size; j++){
+                var p = board[i][j]
+                if(p!=' '){
+                    if(j+k<=size && i+k<=size){                        
+                        var c = 1
+                        for(var h=1;h<k; h++)
+                            if(board[i+h][j+h]==p) c++
+                        if(c==k) return p
+                    }
+                    if(j+1>=k && i+k<=size){                        
+                        var c = 1
+                        for(var h=1;h<k; h++)
+                            if(board[i+h][j-h]==p) c++
+                        if(c==k) return p
+
+                    }
+                    if(j+k<=size){                        
+                        var c = 1
+                        for(var h=1;h<k; h++)
+                            if(board[i][j+h]==p) c++
+                        if(c==k) return p
+
+                    }
+                    if(i+k<=size){
+                        var c = 1
+                        for(var h=1;h<k; h++)
+                            if(board[i+h][j]==p) c++
+                            else break;
+                        if(c==k) return p
+                    }
+                }
+            }
+        }      
+        return ' '
+    }
+
     init(color, board, time = 20000) {
         this.color = color
         this.noColor = color == "W" ? "B" : "W";
@@ -16,24 +55,24 @@ class LineaDCoca extends Agent {
 
     compute(board, time) {
         var moves = this.boardManager.valid_moves(board)
-        var alfa = -Infinity;
-        var beta = Infinity;
-        const index = this.minimax(board, moves, false, 4, alfa, beta)[1];
+        const index = this.alphaBeta(board);
+
+        console.log("maxConsecutiveEnemy: " + this.maxConsecutiveEnemy(board));
+
+        console.log("index: " + index);
 
         console.log(this.color + ',' + moves[index])
         return moves[index]
     }
 
-    conflictos(board, isMaximizingPlayer) {
-        if (tablero[i] == tablero[j] || Math.abs(tablero[i] - tablero[j]) == Math.abs(i - j)) {
-            c++;
-        }
+    evaluate(board, isMaximizingPlayer) {
+        return this.evaluateSingle(board, isMaximizingPlayer) - this.evaluateSingle(board, !isMaximizingPlayer);
     }
 
-    evaluate(board, isMaximizingPlayer) {
+    evaluateSingle(board, isMaximizingPlayer) {
         let n = board.length;
         let colorminmax = isMaximizingPlayer ? this.color : this.noColor;
-        const patronPunto = new RegExp(`[${colorminmax} ]{${this.k},}`, "g")
+        const patronPunto = new RegExp(`[${colorminmax} ]{${this.k},}`, "g");
         let vertical = [];
         let horizontal = [];
         let diagonalI = [];
@@ -102,53 +141,109 @@ class LineaDCoca extends Agent {
 
             vertical = []; horizontal = []; diagonalI = []; diagonalS = [];
         }
-
-
+        
         return (conteoVertical + conteoHorizontal + conteoDiagonalS + conteoDiagonalI);
     }
 
-
-
-
-    minimax(board, moves, isMaximizingPlayer,depth, alpha, beta, index = 0) {
-        if (depth === 0 || moves.length === 0) {
-            let mievaluacion = this.evaluate(board,isMaximizingPlayer);
-            console.log("mi evaluacion: ",mievaluacion);
-            return [mievaluacion, index];
+    alphaBeta(board) {
+        let evaluateIndex = this.minValue(board, -Infinity, Infinity)[1];
+        if (evaluateIndex < 0) {
+            console.log("random");
+            return Math.floor(Math.random() * this.boardManager.valid_moves(board).length);
+        }else{
+            console.log("alphaBeta");
+            return evaluateIndex;
         }
-    
-        if (isMaximizingPlayer) {
-            let bestScore = -Infinity;
-            for (let i=0; i < moves.length; i++) {
-                const cloneBoard = this.boardManager.clone(board);
-                this.boardManager.move(cloneBoard, moves[i], this.color);
-                const movesF = this.boardManager.valid_moves(cloneBoard) ;
-                const cosas = this.minimax(cloneBoard, movesF, false, depth - 1, alpha, beta, i);
-                const score = cosas[0];
-                bestScore = Math.max(bestScore, score);
-                index = cosas[1];
-                alpha = Math.max(alpha, score);
-                if (beta <= alpha) {
-                    break; // Poda alfa-beta
+    }
+
+    maxConsecutiveEnemy(board) {
+        let n = board.length;
+        let colorminmax = this.noColor;
+        const patronPunto = new RegExp(`[${colorminmax}]{2,}`, "g");
+        let vertical = [];
+        let horizontal = [];
+        let diagonalI = [];
+        let diagonalS = [];
+
+        let conteo = 0;
+
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                horizontal.push(board[i][j]);
+                vertical.push(board[j][i]);
+
+                if (i + j < n) {
+                    diagonalI.push(board[i][i + j]);
+                    diagonalS.push(board[i + j][i]);
                 }
+
             }
-            return [bestScore, index];
-        } else {
-            let bestScore = Infinity;
-            for (let i=0; i < moves.length; i++) {
-                const cloneBoard = this.boardManager.clone(board);
-                this.boardManager.move(cloneBoard, moves[i], this.noColor); 
-                const movesF = this.boardManager.valid_moves(cloneBoard);
-                const cosas = this.minimax(cloneBoard, movesF, true, depth - 1, alpha, beta, i);
-                const score = cosas[0];
-                index = cosas[1];
-                bestScore = Math.min(bestScore, score);
-                beta = Math.min(beta, score);
-                if (beta <= alpha) {
-                    break; // Poda alfa-beta
-                }
-            }
-            return [bestScore, index];
+
+            let verticalStr = vertical.join("");
+            let horizontalStr = horizontal.join("");
+            let diagonalIStr = diagonalI.join("");
+            let diagonalSStr = diagonalS.join("");
+
+            const coincidenciaVer = verticalStr.match(patronPunto)?.map(function (s) { return s.length; });
+            const coincidenciaHor = horizontalStr.match(patronPunto)?.map(function (s) { return s.length; });
+            const coincidenciaDiagI = diagonalIStr.match(patronPunto)?.map(function (s) { return s.length; });
+            const coincidenciaDiagS = diagonalSStr.match(patronPunto)?.map(function (s) { return s.length; });
+            
+            console.log("coincidenciaVer: " + coincidenciaVer);
+            console.log("coincidenciaHor: " + coincidenciaHor);
+            console.log("coincidenciaDiagI: " + coincidenciaDiagI);
+            console.log("coincidenciaDiagS: " + coincidenciaDiagS);
+
+            const conteoVertical = coincidenciaVer ? coincidenciaVer.reduce(function(max, length) {return Math.max(max, length);}, 0) : 0;
+            const conteoHorizontal = coincidenciaHor ? coincidenciaHor.reduce(function(max, length) {return Math.max(max, length);}, 0) : 0;
+            const conteoDiagonalI = coincidenciaDiagI ? coincidenciaDiagI.reduce(function(max, length) {return Math.max(max, length);}, 0) : 0;
+            const conteoDiagonalS = coincidenciaDiagS ? coincidenciaDiagS.reduce(function(max, length) {return Math.max(max, length);}, 0) : 0;
+            
+            conteo = Math.max(conteoVertical, conteoHorizontal, conteoDiagonalI, conteoDiagonalS);
         }
+
+        return conteo;
+    }
+
+    maxValue(board, alpha, beta) {
+        if (this.winner(board, 2) != ' ') {
+            return [this.evaluate(board, true),-1];
+        }
+        let moves = this.boardManager.valid_moves(board);
+        let v = -Infinity;
+        let u = -1;
+        for (let i=0; i < moves.length; i++) {
+            const cloneBoard = this.boardManager.clone(board);
+            this.boardManager.move(cloneBoard, moves[i], this.color);
+            let minvalue = this.minValue(cloneBoard, alpha, beta);
+            v = Math.max(v,minvalue[0]);
+            u = (v == minvalue[0]) ? moves[i] : u;
+            if (v >= beta) {
+                return [v, u]; 
+            }
+            alpha = Math.max(alpha, v);
+        }
+        return [v, u];
+    }
+
+    minValue(board, alpha, beta) {
+        if (this.winner(board, 2) != ' ') {
+            return [this.evaluate(board, true),-1];
+        }
+        let moves = this.boardManager.valid_moves(board);
+        let v = Infinity;
+        let u = -1;
+        for (let i=0; i < moves.length; i++) {
+            const cloneBoard = this.boardManager.clone(board);
+            this.boardManager.move(cloneBoard, moves[i], this.noColor);
+            let maxvalue = this.maxValue(cloneBoard, alpha, beta); 
+            v = Math.min(v, maxvalue[0]);
+            u = (v == maxvalue[0]) ? moves[i] : u;
+            if (v >= alpha) {
+                return [v, u]; 
+            }
+            beta = Math.min(beta, v);
+        }
+        return [v, u];
     }
 }
