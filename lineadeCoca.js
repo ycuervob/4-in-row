2c49a6d55b1d97154823b2efcc19c0376155966a
dvillamor0 <dvillamor@unal.edu.co>
2023-10-31 21:48:41 -0500

    muchos TODOs

--- lineadeCoca.js
+++ lineadeCoca.js
@@ -18,19 +18,36 @@ class LineaDCoca extends Agent {
         let index = 0;
         let moves = this.boardManager.valid_moves(board);
 
-        //mejor movimiento fino
-        const iFino = this.searchBestMovement(board,moves,this.color);
+        index = this.algoritmo(board,moves);
+        
+        return moves[index];
+    }
+
+    algoritmo(board,moves){
+        //indices movimientos ordenados fino
+        const ImovesFino = this.searchBestMovement(board,moves,this.color);
+        let iFino = ImovesFino.shift();
         const clonTablero = this.boardManager.clone(board);
 
+        if (!iFino) {
+            return 0
+        }
 
-        //mejor movimiento choto
+
+        //indices movimientos ordenados choto
         this.boardManager.move(clonTablero,moves[iFino],this.noColor);
         moves = this.boardManager.valid_moves(clonTablero);
-        const iChoto = this.searchBestMovement(clonTablero,moves,this.noColor);
+        const imovesChoto = this.searchBestMovement(clonTablero,moves,this.noColor);
+        const iChoto = imovesChoto[0];
 
-        let indexRandom = 9;
-        index = iFino ? ( iChoto ? iChoto : indexRandom ) :  indexRandom;
-        return moves[index];
+        if (iChoto) {
+            if (iFino == iChoto) {
+                this.algoritmo(board,ImovesFino);
+            }else{
+                iFino = iChoto;
+            }
+        }
+        return iFino;
     }
 
     //da las posibilidades que tiene colorminmax para juntar fichas
@@ -44,12 +61,14 @@ class LineaDCoca extends Agent {
                     let c2 = 0;
                     let c3 = 0;
                     let c4 = 0;
+                    //diagonales 1
                     if(j+k<=size && i+k<=size){                        
                         c1 = 1
                         for(var h=1;h<k; h++)
                             if(board[i+h][j+h]==colorminmax) c1++
                         if(c1==k) return Infinity
                     }
+                    //diagonales 2
                     if(j+1>=k && i+k<=size){                        
                         c2 = 1
                         for(var h=1;h<k; h++)
@@ -57,6 +76,7 @@ class LineaDCoca extends Agent {
                         if(c2==k) return Infinity
 
                     }
+                    //filas
                     if(j+k<=size){                        
                         c3 = 1
                         for(var h=1;h<k; h++)
@@ -64,6 +84,7 @@ class LineaDCoca extends Agent {
                         if(c3==k) return Infinity
 
                     }
+                    //columnas
                     if(i+k<=size){
                         c4 = 1
                         for(var h=1;h<k; h++)
@@ -71,6 +92,7 @@ class LineaDCoca extends Agent {
                             else break;
                         if(c4==k) return Infinity
                     }
+                    console.log("color",colorminmax,"dia1",c1,"dia2",c2,"filas",c3,"col",c4);
                     return c1+c2+c3+c4
                 }
             }
@@ -78,21 +100,25 @@ class LineaDCoca extends Agent {
         return ' '
     }
 
+    //TODO organiza los movimientos de mejor a peor
     searchBestMovement(board,moves,colorminmax){
+        mejorResultado = -Infinity;
+        let index = -1;
         for (let i = 0; i < moves.length; i++) {
             const move = moves[i];
             const clonTablero = this.boardManager.clone(board);
             this.boardManager.move(clonTablero,move,colorminmax);
             const resultado = this.evaluate(clonTablero, this.k, colorminmax);
+            if (resultado == Infinity) {
+                //TODO avisar y actuar en consecuencia
+            }
             console.log("resultado", resultado);
             if (resultado !== " ") {
-                return i
-            }else{
-                return false
-            }
-            
+                if (resultado >= mejorResultado) {
+                    index = i;
+                }
+            }            
         }
-        return false;
     }
 
 }
